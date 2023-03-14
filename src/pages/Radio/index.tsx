8fddd4d0acd3978293c71fdd607597a96722b60f
xuzhida <xuzhida@163.com>
2023-03-14 16:07:41 +0800

    fix

--- src/pages/Radio/index.tsx
+++ src/pages/Radio/index.tsx
@@ -33,9 +33,9 @@ let wakeLock: any = null;
 const TimeLineItem = memo(({ e, jumpTime, isChoose }: { e: any, jumpTime: Function, isChoose?: boolean }) => {
   return (
     // <Tooltip placement="top" content={e.attributes.time}>
-      <div className={" w-10 h-32 inline-block bg-[#35363a]  pt-1 [writing-mode:vertical-lr] text-ellipsis overflow-hidden whitespace-nowrap hover:bg-slate-400 " + (isChoose || false ? 'bg-slate-600' : '')} onClick={() => { jumpTime(e) }} title={e.attributes.time} >
-        {e.attributes.title}
-      </div>
+    <div className={" w-10 h-32 inline-block bg-[#35363a]  pt-1 [writing-mode:vertical-lr] text-ellipsis overflow-hidden whitespace-nowrap hover:bg-slate-400 " + (isChoose || false ? 'bg-slate-600' : '')} onClick={() => { jumpTime(e) }} title={e.attributes.time} >
+      {e.attributes.title}
+    </div>
     // </Tooltip>
   )
 })
@@ -142,7 +142,9 @@ const Radio: React.FC<IRadioProp> = forwardRef(({ children }, ref) => {
           setCurTimeItem(nextItem)
         }
       }
-      savePlaylog()
+      if (!audioRef.current?.paused) {
+        savePlaylog()
+      }
     }
     const interval = setInterval(() => {
       doInterval()
